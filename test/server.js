a1ae87afea7766718a178ba13e10372bfbcfef22
Roman Smelyansky <romans@madlan.co.il>
2019-08-30 20:53:56 +0300

    tests

---
+++ test/server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../src/server');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+let port = 3000;
+
+describe('Server', () => {
+    let s = null;
+
+    before(() => {
+        s = server.listen(port, () => console.log(`Listening on port ${port}`));
+    });
+
+    after(() => {
+        s.close();
+    });
+
+    describe('register machine', () => {
+        it('it should register a machine', (done) => {
+            chai.request(server)
+            .post('/registerMachine')
+            .end((err, result) => {
+                result.should.have.status(200);
+            });
+
+            done();
+        })
+    })
+})
